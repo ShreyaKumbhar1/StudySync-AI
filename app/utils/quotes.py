6f8ce5f9b4836b3bfc395facbf55ad6b2e730b76
shreyakumbhar28 <vtu30404@veltech.edu.in>
2026-08-02 18:00:42 +0530

    The app stopped forgetting everything

---
+++ app/utils/quotes.py
@@ -0,0 +1,28 @@
+import random
+
+quotes = [
+
+    "Bestie... your GPA isn't going to increase from positive vibes alone.",
+
+    "POV: You opened Instagram 'for 2 minutes'.",
+
+    "Your future self is begging you to finish that assignment.",
+
+    "If procrastination burned calories, you'd be the campus topper.",
+
+    "Study now. Panic later. Preferably never.",
+
+    "Your attendance called... it's crying.",
+
+    "One chapter today is better than ten tomorrow.",
+
+    "Close Netflix. Open Notes.",
+
+    "You don't need motivation. You need Ctrl + S.",
+
+    "Dimpu believes in you... but your syllabus doesn't."
+
+]
+
+def get_quote():
+    return random.choice(quotes)
--- run.py
+++ run.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
+from app.utils.quotes import get_quote
+from app.database import db
+from app.models.user import User
 
 app = Flask(
     __name__,
     template_folder="app/templates",
     static_folder="app/static"
 )
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///studysync.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db.init_app(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    return render_template(
+        "index.html",
+        quote=get_quote()
+    )
 
 @app.route("/dashboard")
 def dashboard():
@@ -46,6 +54,17 @@ def recommendation():
 def profile():
     return render_template("profile.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
+    
