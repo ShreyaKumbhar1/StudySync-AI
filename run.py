182c1af94e20e66de90234ae183aa26354224559
shreyakumbhar28 <vtu30404@veltech.edu.in>
2026-08-03 22:37:21 +0530

    Plans change, thankfully the app agrees

--- run.py
+++ run.py
@@ -1,30 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
 from app.utils.quotes import get_quote
 from app.database import db
+
 from app.models.user import User
+from app.models.task import Task
+
+from app.forms.task_form import TaskForm
+
 from app.routes.auth import auth
+
 from app.extensions import bcrypt
 from app.login_manager import login_manager
+
 from flask_login import login_required, current_user
-from app.models.task import Task
-from app.forms.task_form import TaskForm
-from app.models.task import Task
-from flask import redirect, url_for
-from flask_login import login_required, current_user
+
 
 app = Flask(
     __name__,
     template_folder="app/templates",
     static_folder="app/static"
 )
-bcrypt.init_app(app)
+
 app.config["SECRET_KEY"] = "change-this-later"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///studysync.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+bcrypt.init_app(app)
 db.init_app(app)
 login_manager.init_app(app)
+
 app.register_blueprint(auth)
+
 
 @app.route("/")
 def home():
@@ -33,6 +40,7 @@ def home():
         quote=get_quote()
     )
 
+
 @app.route("/dashboard")
 @login_required
 def dashboard():
@@ -40,6 +48,7 @@ def dashboard():
         "dashboard.html",
         user=current_user
     )
+
 
 @app.route("/planner", methods=["GET", "POST"])
 @login_required
@@ -73,6 +82,49 @@ def planner():
     )
 
 
+@app.route("/delete-task/<int:task_id>")
+@login_required
+def delete_task(task_id):
+
+    task = Task.query.get_or_404(task_id)
+
+    if task.user_id != current_user.id:
+        return redirect(url_for("planner"))
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect(url_for("planner"))
+
+@app.route("/edit-task/<int:task_id>", methods=["GET", "POST"])
+@login_required
+def edit_task(task_id):
+
+    task = Task.query.get_or_404(task_id)
+
+    if task.user_id != current_user.id:
+        return redirect(url_for("planner"))
+
+    form = TaskForm(obj=task)
+    form.submit.label.text = "💾 Save Changes"
+
+    if form.validate_on_submit():
+
+        task.title = form.title.data
+        task.subject = form.subject.data
+        task.due_date = form.due_date.data
+        task.priority = form.priority.data
+
+        db.session.commit()
+
+        return redirect(url_for("planner"))
+
+    return render_template(
+        "edit_task.html",
+        form=form,
+        task=task
+    )
+
 @app.route("/assignments")
 def assignments():
     return render_template("assignments.html")
@@ -96,6 +148,7 @@ def recommendation():
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
+
 
 if __name__ == "__main__":
     with app.app_context():
